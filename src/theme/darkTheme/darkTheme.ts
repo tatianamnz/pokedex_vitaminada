091b3f7e52059b4ac1bc4f077fd2819b18a99511
Tatiana Muñoz <tatiana.munoz.romeo@gmail.com>
2024-10-25 01:22:12 +0200

    Cambio versiones

--- src/theme/darkTheme/darkTheme.ts
+++ src/theme/darkTheme/darkTheme.ts
@@ -3,13 +3,14 @@ import { fonts } from "@/theme/foundations/fonts";
 import { spaces } from "@/theme/foundations/spaces";
 import { Theme } from "@/theme/theme";
 
-export const darkTheme: Theme = {
-	colors: colors,
-	fonts: fonts,
-	spaces: spaces,
-	themeColors: {
-		mainColor: colors.grayScaleE,
-		secondaryColor: colors.grayScaleC,
-		textColor: colors.grayScaleA,
-	},
-};
+interface ThemeColors {
+	mainColor: string;
+	secondaryColor: string;
+	textColor: string;
+	hoverColor?: string;               // Propiedad opcional
+	disabledColor?: string;            // Propiedad opcional
+	errorBackgroundColor?: string;     // Propiedad opcional
+	errorTextColor?: string;           // Propiedad opcional
+	errorHoverColor?: string;          // Propiedad opcional
+}
+
